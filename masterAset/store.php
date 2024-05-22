04944d983f8a1951d87c3ac41429dfa8aae3cdd4
Angga <agungangga2001@gmail.com>
2024-05-22 15:42:12 +0800

    versi awal create dan edit aset

--- masterAset/store.php
+++ masterAset/store.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
 require_once '../helper/connection.php';
-// require_once '../vendor/autoload.php';
-
-// use BaconQrCode\Renderer\ImageRenderer;
-// use BaconQrCode\Renderer\Image\ImagickImageBackEnd;
-// use BaconQrCode\Renderer\RendererStyle\RendererStyle;
-// use BaconQrCode\Writer;
 
 $namaAset = $_POST['namaAset'];
 $totalBarang = $_POST['totalBarang'];
@@ -19,28 +13,7 @@ $tanggalPembelian = $_POST['tanggalPembelian'];
 $garansi = $_POST['garansi'];
 $deskripsi = $_POST['deskripsi'];
 
-// $qrquery = "Nama Aset         : {$row['namaAset']}
-// Total Barang      : {$row['totalBarang']}
-// Lokasi Aset       : {$row['lokasiAset']}
-// Jenis Aset        : {$row['jenisAset']}
-// Tipe Aset         : {$row['tipeAset']}
-// Supplier          : {$row['supplier']}
-// Harga             : {$row['harga']}
-// Tanggal Pembelian : {$row['tanggalPembelian']}
-// Garansi           : {$row['garansi']}
-// Deskripsi         : {$row['deskripsi']}";
 $query = mysqli_query($connection, "insert into dataaset(namaAset, totalBarang, lokasiAset, jenisAset, tipeAset, supplier, harga, tanggalPembelian, garansi, deskripsi) value('$namaAset', '$totalBarang', '$lokasiAset', '$jenisAset', '$tipeAset', '$supplier', '$harga', '$tanggalPembelian', '$garansi', '$deskripsi')");
-
-// $renderer = new ImageRenderer(
-//   new RendererStyle(150),
-//   new ImagickImageBackEnd()
-// );
-// $writer = new Writer($renderer);
-// $writer->writeFile($qrquery, 'storage/qrcode.png');
-
-// $qrCodeImage = 'storage/qrcode.png';
-// $qrCodeBase64 = base64_encode(file_get_contents($qrCodeImage));
-
 
 if ($query) {
   $_SESSION['info'] = [
